edae7508455bda1930a2e590d10c0a712f46fb8f
farhang-sa <Saeedi.Farhang@gmail.com>
2024-08-25 10:40:33 +0330

    Utility functions added. Interface names changed.

--- MaxTools.php
+++ MaxTools.php
@@ -1,6 +1,18 @@
-<?php 
-include_once 'Crypt/AESCrypt.php' ;
-include_once 'DbManager/Server.php' ;
-include_once 'Payment/interface.php' ;
-include_once 'SMS/interface.php' ;
+<?php namespace MaxTools ;
+
+if( ! class_exists( 'MaxCrypt\AESCrypt' ) )
+	include_once 'Crypt/AESCrypt.php' ;
+
+if( ! class_exists( 'MaxDatabaseManager\Server' ) )
+	include_once 'DbManager/Server.php' ;
+
+if( ! interface_exists( 'MaxPayment\Payment' ) )
+	include_once 'Payment/Payment.php' ;
+
+if( ! class_exists( 'MaxSMS\SMS' ) )
+	include_once 'SMS/SMS.php' ;
+
+if( ! function_exists( 'MaxTools\_DS' ) )
+	include_once 'Utilities/Functions.php' ;
+
 ?>
