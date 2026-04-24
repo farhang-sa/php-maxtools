4515c76207b89052479d6ac818ed0b33f6ef681f
farhang-sa <Saeedi.Farhang@gmail.com>
2026-04-24 18:12:19 +0330

    Updated MaxCrypt/AESCrypt for more flex in ssl en/de cryption

--- Crypt/AESCrypt.php
+++ Crypt/AESCrypt.php
@@ -2,68 +2,88 @@
 
 class AESCrypt {
 
-   	public static function SSL_Encrypt( $DataToEnCrypt , $Key , $algorithm = 'aes-128-cbc' ) {
-	   	
-	   	if ( $DataToEnCrypt == null || $Key == null || ! is_string( $Key ) ) return null ;
-	  
-	  	if ( strlen( $Key ) != 32 ) return null ;
-       
-       	try { 
+	private static $default_crypto_iv = null ;
 
-       		return openssl_encrypt( $DataToEnCrypt , $algorithm , $Key ) ; 
+	public static function setDefaultIV( $iv = '!1275K036#8@A9+4' ){
+        $letters = '1234567890-!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' ;
+	    while( strlen($iv) < 16 )
+	        $iv .= substr($letters , rand(0,strlen($letters)-1) , 1);
+        self::$default_crypto_iv = $iv ;
+	}
+
+   	public static function SSL_Encrypt( $DataToEnCrypt , $Key , $algo = 'aes-256-cbc' , $iv = null ){
+
+	   	if ( $DataToEnCrypt == null || $Key == null || ! is_string( $Key ) )
+	   	    return null ;
+
+	  	if ( strlen( $Key ) != 32 )
+	  	    return null ;
+
+        if( $algo === null )
+            $algo = 'aes-256-cbc' ;
+
+       	try {
+
+       		$iv = $iv ? $iv : self::$default_crypto_iv ;
+
+       		return openssl_encrypt( $DataToEnCrypt , $algo , $Key , 0 , $iv ) ;
 
        	} catch (Exception $e) {} return null ;
    }
 
-   	public static function SSL_Decrypt( $DataToDeCrypt , $Key , $algorithm = 'aes-128-cbc' ){
+   	public static function SSL_Decrypt( $DataToDeCrypt , $Key , $algo = 'aes-256-cbc' , $iv = null ){
 
-	   	if ( $DataToDeCrypt == null || $Key == null || ! is_string( $Key ) ) return null ;
+	   	if ( $DataToDeCrypt == null || $Key == null || ! is_string( $Key ) )
+	   	    return null ;
 
-	  	if ( strlen( $Key ) != 32 ) return null ;
-       
-       	try { 
+	  	if ( strlen( $Key ) != 32 )
+	  	    return null ;
 
-       		return openssl_decrypt( $DataToDeCrypt , $algorithm , $Key ) ; 
+        if( $algo === null )
+            $algo = 'aes-256-cbc' ;
+
+       	try {
+
+       		$iv = $iv ? $iv : self::$default_crypto_iv ;
+
+       		return openssl_decrypt( $DataToDeCrypt , $algo , $Key , 0 , $iv ) ;
 
        	} catch (Exception $e) {} return null ;
 
-   }
+    }
 
-
+    // deprecated
    	public static function AES_ECB_PKCS5_Encrypt( $DataToEnCrypt , $Key ) {
-	   	
+
 	   	if ( $DataToEnCrypt == null || $Key == null || ! is_string( $Key ) ) return null ;
-	  
+
 	  	if ( strlen( $Key ) != 32 ) return null ;
-       
-       	try { 
+
+       	try {
 
        		if( function_exists( 'mcrypt_encrypt' ) )
-
-       			return mcrypt_encrypt( MCRYPT_RIJNDAEL_128 , $Key , $DataToEnCrypt , MCRYPT_MODE_ECB ) ; 
+       			return mcrypt_encrypt( MCRYPT_RIJNDAEL_128 , $Key , $DataToEnCrypt , MCRYPT_MODE_ECB ) ;
 
        		if( function_exists( 'openssl_encrypt' ) )
-
-       			return openssl_encrypt( $DataToEnCrypt , 'aes-128-ecb' , $Key ) ; 
+       			return self::SSL_Encrypt( $DataToEnCrypt , $Key ) ;
 
        	} catch (Exception $e) {} return null ;
    }
 
+    // deprecated
    	public static function AES_ECB_PKCS5_Decrypt( $DataToDeCrypt , $Key ){
 
 	   	if ( $DataToDeCrypt == null || $Key == null || ! is_string( $Key ) ) return null ;
 
 	  	if ( strlen( $Key ) != 32 ) return null ;
-       
-       	try { 
+
+       	try {
 
        		if( function_exists( 'mcrypt_decrypt' ) )
-
-       			return mcrypt_decrypt( MCRYPT_RIJNDAEL_128 , $Key , $DataToDeCrypt , MCRYPT_MODE_ECB ) ; 
+       			return mcrypt_decrypt( MCRYPT_RIJNDAEL_128 , $Key , $DataToDeCrypt , MCRYPT_MODE_ECB ) ;
 
        		if( function_exists( 'openssl_decrypt' ) )
-
-       			return openssl_decrypt( $DataToDeCrypt , 'aes-128-ecb' , $Key ) ; 
+       			return self::SSL_Decrypt( $DataToDeCrypt , $Key ) ;
 
        	} catch (Exception $e) {} return null ;
 
